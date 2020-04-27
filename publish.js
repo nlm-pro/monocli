bb436784956e80026d8a59437f393ca55e6818dc
Noel Mace <contact@noelmace.com>
2020-04-28 00:01:11 +0200

    build: publish prereleases with @next tag

--- publish.js
+++ publish.js
@@ -1,8 +1,11 @@
 const fs = require("fs-extra");
 const { publish } = require("libnpmpublish");
 const readline = require("readline");
+const { prerelease } = require("semver");
 
-(async () => {
+const args = process.argv.slice(2);
+
+(async ([dryRun]) => {
   const pkgJsonString = await fs.readFile("package.json", "utf8");
   const pkgJson = JSON.parse(pkgJsonString);
   const publishPkgJson = { ...pkgJson };
@@ -22,17 +25,36 @@ const readline = require("readline");
   const token = await ask(`token: `);
   rl.close();
 
+  const defaultTag = prerelease(pkgJson.version) ? "next" : "latest";
+
   try {
-    await fs.writeFile("package.json", JSON.stringify(publishPkgJson, null, 2), { encoding: "utf8" });
-    const res = await publish(__dirname, publishPkgJson, {
-      access: `public`,
-      token
-    });
-    console.log(res);
+    await fs.writeFile(
+      "package.json",
+      JSON.stringify(publishPkgJson, null, 2),
+      { encoding: "utf8" }
+    );
+    if (!dryRun) {
+      const res = await publish(__dirname, publishPkgJson, {
+        access: `public`,
+        defaultTag,
+        token
+      });
+      console.log(res);
+    } else {
+      console.log("== package.json ==");
+      console.log(JSON.stringify(publishPkgJson, null, 2));
+      console.log();
+      console.log("== options ==");
+      console.log({
+        access: `public`,
+        defaultTag,
+        token
+      });
+    }
   } catch (err) {
     console.error(err.message);
     process.exitCode = 1;
   } finally {
     await fs.writeFile("package.json", pkgJsonString, { encoding: "utf8" });
   }
-})();
+})([args.includes("--dry-run")]);
